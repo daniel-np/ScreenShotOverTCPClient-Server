36531b99846e4a135a82c7190bb31eb7bbc94843
Daniel pettersen <d.pettersen@gmail.com>
2020-02-23 14:01:36 +0000

    Added a separate entry point for text menu, with args -t or -text

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,56 +1,7 @@
-import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-import service.screenShot.Screenshot;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import ui.fx.MainStage;
 import ui.text.TextMenu;
 
-import java.awt.image.BufferedImage;
-
-public class Main extends Application {
-    @Override
-    public void start(Stage stage) {
-
-        //Setting title to the Stage
-        stage.setTitle("ScreenShot App");
-
-        //Adding scene to the stage
-        Scene screenShotScene = mainScene();
-        screenShotScene.setFill(Color.BEIGE);
-        stage.setScene(screenShotScene);
-
-        //Displaying the contents of the stage
-        stage.show();
-    }
-
-    private Scene mainScene() {
-        //Creating an image
-
-        //Setting the image view
-        ImageView imageView = new ImageView(takeScreenShot());
-
-        //Setting the position of the image
-        imageView.setX(50);
-        imageView.setY(25);
-
-        //setting the fit height and width of the image view
-        imageView.setFitHeight(450);
-        imageView.setFitWidth(500);
-
-        //Setting the preserve ratio of the image view
-        imageView.setPreserveRatio(true);
-
-        //Creating a Group object
-        Group root = new Group(imageView);
-
-        //Creating a scene object
-        return new Scene(root, 600, 400);
-    }
-
+public class Main {
 
     public static void main(String[] args) {
         if(args.length > 0) {
@@ -61,16 +12,8 @@ public class Main extends Application {
                 System.out.println("Invalid argument");
                 System.exit(0);
             }
+        } else {
+            MainStage.main(args);
         }
-        //launch();
     }
-
-    private Image takeScreenShot() {
-        BufferedImage screenShot  = Screenshot.captureWholeScreen();
-        String path = System.getProperty("user.home")+"/Screenshot.png";
-        Screenshot.saveImage(path, screenShot);
-
-        return new Image("file:" + path);
-    }
-
 }
--- src/main/java/ui/fx/MainStage.java
+++ src/main/java/ui/fx/MainStage.java
@@ -1,18 +1,62 @@
 package ui.fx;
 
 import controllers.UiController;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import service.screenShot.Screenshot;
 
 import java.awt.image.BufferedImage;
 
-public class MainStage extends Stage {
+public class MainStage extends Application {
 
     UiController uiController = new UiController();
 
-    public MainStage() {
+    public static void main(String[] args) {
+        launch(args);
+    }
 
+    @Override
+    public void start(Stage stage) {
+
+        //Setting title to the Stage
+        stage.setTitle("ScreenShot App");
+
+        //Adding scene to the stage
+        Scene screenShotScene = mainScene();
+        screenShotScene.setFill(Color.BEIGE);
+        stage.setScene(screenShotScene);
+
+        //Displaying the contents of the stage
+        stage.show();
+    }
+
+    private Scene mainScene() {
+        //Creating an image
+
+        //Setting the image view
+        ImageView imageView = new ImageView(takeScreenShot());
+
+        //Setting the position of the image
+        imageView.setX(50);
+        imageView.setY(25);
+
+        //setting the fit height and width of the image view
+        imageView.setFitHeight(450);
+        imageView.setFitWidth(500);
+
+        //Setting the preserve ratio of the image view
+        imageView.setPreserveRatio(true);
+
+        //Creating a Group object
+        Group root = new Group(imageView);
+
+        //Creating a scene object
+        return new Scene(root, 600, 400);
     }
 
     private Image takeScreenShot() {
