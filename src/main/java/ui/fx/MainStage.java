ba95ee5044d254a2ee77848222395be19ded7b6d
Daniel pettersen <d.pettersen@gmail.com>
2020-03-02 18:08:44 +0000

    Implements PCL to allow for reactive screenShot output

--- src/main/java/ui/fx/MainStage.java
+++ src/main/java/ui/fx/MainStage.java
@@ -8,21 +8,46 @@ import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import service.screenShot.PCLScreenShot;
 import service.screenShot.ScreenShotHandler;
 
 import java.awt.image.BufferedImage;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 public class MainStage extends Application {
 
-    UiController uiController = new UiController();
+    private UiController uiController;
+    private ImageView screenShotImageView = new ImageView();
+
+    public MainStage() {
+        PCLScreenShotChannel screenShotChannel = new PCLScreenShotChannel();
+        PCLScreenShot observableScreenShot = new PCLScreenShot();
+        observableScreenShot.addPropertyChangeListener(screenShotChannel);
+        uiController = new UiController(observableScreenShot);
+    }
+
+    private class PCLScreenShotChannel implements PropertyChangeListener {
+
+        @Override
+        public void propertyChange(PropertyChangeEvent propertyChangeEvent) {
+            this.setScreenShot((Image) propertyChangeEvent.getNewValue());
+        }
+
+        private void setScreenShot(Image screenShot) {
+            screenShotImageView.setImage(screenShot);
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
@@ -52,24 +77,23 @@ public class MainStage extends Application {
     }
 
     private Group screenShotGroup() {
-        //Creating an image
-
-        //Setting the image view
-        ImageView imageView = new ImageView(takeScreenShot());
-
         //Setting the position of the image
-        imageView.setX(50);
-        imageView.setY(25);
+        screenShotImageView.setX(50);
+        screenShotImageView.setY(25);
 
         //setting the fit height and width of the image view
-        imageView.setFitHeight(450);
-        imageView.setFitWidth(500);
+        screenShotImageView.setFitHeight(450);
+        screenShotImageView.setFitWidth(500);
 
         //Setting the preserve ratio of the image view
-        imageView.setPreserveRatio(true);
+        screenShotImageView.setPreserveRatio(true);
+
+        screenShotImageView.imageProperty();
 
         //Creating a Group object
-        HBox hBox = new HBox(imageView);
+        HBox hBox = new HBox(screenShotImageView);
+        StackPane sp = new StackPane();
+
         hBox.setAlignment(Pos.CENTER);
 
         //Creating a scene object
@@ -96,17 +120,23 @@ public class MainStage extends Application {
                 FXCollections.observableArrayList(UiController.intervalTimer.values());
 
         ChoiceBox<UiController.intervalTimer> choiceBox = new ChoiceBox<>(observableList);
+        choiceBox.setValue(uiController.getScreenShotTimer());
         timerChoiceBoxLabel.setLabelFor(choiceBox);
 
-        choiceBox.setOnAction((a)->{uiController.setScreenShotTimer(choiceBox.getValue());});
+        choiceBox.setOnAction((actionEvent)->{uiController.setScreenShotTimer(choiceBox.getValue());});
 
-
+        Button startButton = new Button("Start");
+        startButton.setOnAction((actionEvent)-> {
+            uiController.startServer(choiceBox.getValue());
+            uiController.startClient(choiceBox.getValue());
+        });
 
         HBox controlHBox = new HBox(
                 addressLabel,
                 addressTextField,
                 timerChoiceBoxLabel,
-                choiceBox);
+                choiceBox,
+                startButton);
         controlHBox.setSpacing(10);
         controlHBox.setAlignment(Pos.CENTER);
         // Parent VBox
@@ -119,7 +149,7 @@ public class MainStage extends Application {
 
     private Image takeScreenShot() {
         BufferedImage screenShot  = ScreenShotHandler.captureWholeScreen();
-        String path = System.getProperty("user.home")+"/ScreenShotHandler.png";
+        String path = System.getProperty("user.home")+"/ScreenShot " + System.currentTimeMillis()/1000 + ".png";
         ScreenShotHandler.saveImage(path, screenShot);
 
         return new Image("file:" + path);
